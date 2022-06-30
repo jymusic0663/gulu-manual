4b35b56a7ccc525244619f3d1841d3934e5803c5
小林囝 <dong@linyudong.cn>
2022-06-30 15:41:51 +0800

    init

---
+++ assets/index.html.96534670.js
@@ -0,0 +1,21 @@
+import { _ as _export_sfc, o as openBlock, c as createElementBlock, b as createBaseVNode, d as createTextVNode } from "./app.123fb2aa.js";
+const _sfc_main = {};
+const _hoisted_1 = /* @__PURE__ */ createBaseVNode("h1", {
+  id: "\u4E3B\u6301\u4EBA\u7BC7",
+  tabindex: "-1"
+}, [
+  /* @__PURE__ */ createBaseVNode("a", {
+    class: "header-anchor",
+    href: "#\u4E3B\u6301\u4EBA\u7BC7",
+    "aria-hidden": "true"
+  }, "#"),
+  /* @__PURE__ */ createTextVNode(" \u4E3B\u6301\u4EBA\u7BC7")
+], -1);
+const _hoisted_2 = [
+  _hoisted_1
+];
+function _sfc_render(_ctx, _cache) {
+  return openBlock(), createElementBlock("div", null, _hoisted_2);
+}
+var index_html = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render], ["__file", "index.html.vue"]]);
+export { index_html as default };
